b38d227a13d768ce283091cce4a2220432f36009
theswolf <chris.geymo@gmail.com>
2013-07-17 14:09:23 +0200

    Base managers

--- JumperDemo/src/com/example/jumperdemo/GameActivity.java
+++ JumperDemo/src/com/example/jumperdemo/GameActivity.java
@@ -1,22 +1,54 @@
 package com.example.jumperdemo;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.io.IOException;
 
-public class GameActivity extends Activity {
+import org.andengine.engine.Engine;
+import org.andengine.engine.LimitedFPSEngine;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.WakeLockOptions;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_game);
-	}
+import com.example.jumperdemo.manager.ResourcesManager;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.game, menu);
-		return true;
-	}
+public class GameActivity extends BaseGameActivity
+{
+	private Camera camera;
+	private ResourcesManager resourcesManager;
+	
+    public EngineOptions onCreateEngineOptions()
+    {
+    	camera = new Camera(0, 0, 800, 480);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(800, 480), this.camera);
+        engineOptions.getAudioOptions().setNeedsMusic(true).setNeedsSound(true);
+        engineOptions.setWakeLockOptions(WakeLockOptions.SCREEN_ON);
+        return engineOptions;
+    }
+    
+    @Override
+    public Engine onCreateEngine(EngineOptions pEngineOptions) 
+    {
+        return new LimitedFPSEngine(pEngineOptions, 60);
+    }
 
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws IOException
+    {
+    	ResourcesManager.prepareManager(mEngine, this, camera, getVertexBufferObjectManager());
+        resourcesManager = ResourcesManager.getInstance();
+        pOnCreateResourcesCallback.onCreateResourcesFinished();
+        
+    }
+
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws IOException
+    {
+        
+    }
+
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException
+    {
+        
+    }
 }
